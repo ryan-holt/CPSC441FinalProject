f1cec22c0b374b97f4d067145887941ed089557d
R Lee <alpha.r.lee.dev@gmail.com>
2019-11-01 13:46:02 -0600

    UNFINISHED adding testing.

--- src/util/SocketHandler.java
+++ src/util/SocketHandler.java
@@ -1,4 +1,53 @@
 package util;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+
 public class SocketHandler {
+	private ObjectOutputStream objectWriter;
+	private ObjectInputStream objectReader;
+
+	public SocketHandler(Socket socket) {
+		try {
+			//Create the IO object streams. objectWriter MUST come before objectReader
+			objectWriter = new ObjectOutputStream(socket.getOutputStream());
+			objectReader = new ObjectInputStream(socket.getInputStream());
+		} catch (IOException e) {
+			System.err.println("Error: Could not create SocketHandler");
+			e.printStackTrace();
+		}
+	}
+
+	public Message read() {
+		Message msg = null;
+
+		try {
+			msg = (Message) objectReader.readObject();
+		} catch (SocketException e) {
+			//e.printStackTrace();
+			System.out.println("SocketException: Connection must have reset. Bye!");
+		} catch (EOFException e) {
+			//Ignore. Be quiet my child
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.err.println("Could not read message from socket");
+			e.printStackTrace();
+		}
+
+		return msg;
+	}
+
+	public void write(Message msg) {
+		try {
+			objectWriter.writeObject(msg);
+		} catch (IOException e) {
+			System.err.println("Error: SocketHandler failed to write to socket");
+			e.printStackTrace();
+		}
+	}
 }
---
+++ test/util/SocketHandlerTest.java
@@ -0,0 +1,23 @@
+package util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SocketHandlerTest {
+
+//	@BeforeEach
+//	void setUp() {
+//	}
+
+	@Test
+	void read() {
+
+		SocketHandler socketHandler;
+	}
+
+	@Test
+	void write() {
+	}
+}
