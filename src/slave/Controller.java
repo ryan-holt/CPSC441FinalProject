9af74d65a9bd9d2e6bfd76fd9ab6cdb681d218b9
R Lee <alpha.r.lee.dev@gmail.com>
2019-11-02 05:29:51 -0600

    Started slave framework.

---
+++ src/slave/Controller.java
@@ -0,0 +1,17 @@
+package slave;
+
+public class Controller {
+	public void handleMessage() {
+		String action = "requestAssociationRules"; // TODO hook into Message input
+		switch (action) {
+			case "requestAssociationRules":
+				break;
+			default:
+				// TODO Throw exception
+		}
+	}
+
+	private void calculateAssociationRules() {
+
+	}
+}
