856d4b884d645282d2c62b7cfe262b8352bf4272
garywu2 <gary.wu2@ucalgary.ca>
2019-11-01 18:23:53 -0600

    Sockets for Server and Client

---
+++ src/ServerController.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * This class is responsible for managing the thread pool as well as
+ * the ServerCommunicationController. Everytime, a new client connects,
+ * this class makes an instance of the ServerCommunicationController
+ * for the client in a new thread
+ */
+public class ServerController {
+
+    private static final int PORT = 9000;
+    private ServerSocket serverSocket;
+    private ExecutorService pool;
+
+    public ServerController() {
+        try {
+            serverSocket = new ServerSocket(PORT);
+            pool = Executors.newFixedThreadPool(10);
+            System.out.println("Server is running");
+            printIPInfo();
+            System.out.println("********");
+        } catch (IOException e) {
+            System.out.println("ServerController: Create a new socket error");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ServerController myServer = new ServerController();
+        myServer.communicateWithClient();
+    }
+
+    public void communicateWithClient() {
+        try {
+            while (true) {
+                SocketHandler scc = new SocketHandler(serverSocket.accept(), this);
+
+                System.out.println("New Client Connected");
+
+                pool.execute(scc);
+            }
+        } catch (IOException e) {
+            System.out.println("ServerController: CommunicateWithClient error");
+            e.printStackTrace();
+        }
+    }
+
+    public void printIPInfo() {
+        InetAddress ip;
+        try {
+            ip = InetAddress.getLocalHost();
+            System.out.println("You current IP address: " + ip);
+        } catch (UnknownHostException e) {
+            System.out.println("IP Print error");
+            e.printStackTrace();
+        }
+    }
+
+}
