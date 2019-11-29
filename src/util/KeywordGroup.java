b35ac8a7f4eb7587952a472a23e6216bc0aa9f04
R Lee <alpha.r.lee.dev@gmail.com>
2019-11-29 00:28:03 -0700

    Added similar functions to KeywordGroup and RulesCorrelation

--- src/util/KeywordGroup.java
+++ src/util/KeywordGroup.java
@@ -1,12 +1,13 @@
 package util;
 
-import javax.crypto.KeyGenerator;
 import java.io.Serializable;
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 
-public class KeywordGroup implements Serializable {
+public class KeywordGroup implements Serializable, Comparable<KeywordGroup> {
 	private ArrayList<String> keywords;
 
 	public KeywordGroup(ArrayList<String> keywords) {
@@ -32,6 +33,43 @@ public class KeywordGroup implements Serializable {
 	@Override
 	public int hashCode() {
 		return keywords.hashCode();
+	}
+
+	/**
+	 * Compares this to another object
+	 * @param obj
+	 * @return true if the other object is either this object or is a KeywordGroup and has the same keywords as this does
+	 */
+	public boolean similar(Object obj) {
+		if (obj == this) {
+			return true;
+		} else if (!(obj instanceof  KeywordGroup)) {
+			return false;
+		}
+
+		ArrayList<String> keywords1 = new ArrayList<>(keywords);
+		ArrayList<String> keywords2 = new ArrayList<>(((KeywordGroup) obj).keywords);
+		Collections.sort(keywords1);
+		Collections.sort(keywords2);
+
+		return keywords1.equals(keywords2);
+	}
+
+	public int compareTo(KeywordGroup other) {
+		ArrayList<String> keywords1 = new ArrayList<>(keywords);
+		ArrayList<String> keywords2 = new ArrayList<>(other.keywords);
+		Collections.sort(keywords1);
+		Collections.sort(keywords2);
+
+		if (keywords1.isEmpty() && keywords2.isEmpty()) {
+			return 0;
+		} else if (keywords1.isEmpty()) {
+			return -1;
+		} else if (keywords2.isEmpty()) {
+			return 1;
+		}
+
+		return keywords1.get(0).compareTo(keywords2.get(0));
 	}
 
 	public void sort() {
--- src/util/RulesCorrelation.java
+++ src/util/RulesCorrelation.java
@@ -2,6 +2,9 @@ package util;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class RulesCorrelation implements Serializable {
     private ArrayList<KeywordGroup> keywordGroups;
@@ -34,6 +37,36 @@ public class RulesCorrelation implements Serializable {
         }
         s.append(": ").append(score);
         return s.toString();
+    }
+
+    public boolean similar(Object obj) {
+        if (obj == this) {
+            return true;
+        } else if (!(obj instanceof RulesCorrelation)) {
+            return false;
+        }
+
+        RulesCorrelation other = (RulesCorrelation) obj;
+
+        if (score != other.score) {
+             return false;
+        }
+
+        ArrayList<KeywordGroup> groupsCopy1 = new ArrayList<>(keywordGroups);
+        ArrayList<KeywordGroup> groupsCopy2 = new ArrayList<>(other.keywordGroups);
+        Collections.sort(groupsCopy1);
+        Collections.sort(groupsCopy2);
+
+        Iterator<KeywordGroup> it1 = groupsCopy1.iterator();
+        Iterator<KeywordGroup> it2 = groupsCopy2.iterator();
+
+        while (it1.hasNext() && it2.hasNext()) {
+            if (!it1.next().similar(it2.next())) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public double getScore() {
